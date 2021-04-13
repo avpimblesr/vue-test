98b36e09ad0fb92d69d23618871b6386a9d1e698
avpimblesr <allan.pimble@gmail.com>
2021-04-13 15:22:01 -0400

    Added data

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-//  application root
+//Add an array of sizes to the data and use v-for to display them in a list.
 
 var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of fuzzie socks.',
-    image: '/img/vmSocks-green-onWhite.jpg',
-    link:'https://www.amazon.com/s/ref-nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+    image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+    inStock: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+      },
+    ],
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
   },
 })
-
