3fbb0094888a063943f17a47fdae92e328303a8b
avpimblesr <allan.pimble@gmail.com>
2021-04-13 14:36:15 -0400

    Started product page

---
+++ main.js
@@ -0,0 +1,12 @@
+//  application root
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'A pair of fuzzie socks.',
+    image: '/img/vmSocks-green-onWhite.jpg',
+    link:'https://www.amazon.com/s/ref-nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+  },
+})
+
